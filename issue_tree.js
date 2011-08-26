a9ce99d70342e66f8778fbd3175d43fbfefb8c20
Kornel Lugosi <coornail@gmail.com>
2011-08-26 11:11:06 +0100

    Add chrome extension files

---
+++ issue_tree.js
@@ -0,0 +1,50 @@
+var issue_tree = {
+  /**
+   * Parses the issue body searching for drupal.org issues.
+   */
+
+  // jQuery selector for the issue links (and possibly other links)
+  issue_link_selector: '#content-inner span a',
+
+  // Regex for parsing out only the issue queue references
+  issue_link_selector_regex: '^#[0-9]*',
+
+  /**
+   * Get the issue numbers from the issue body.
+   *
+   * @return array
+   *   Array of issue numbers.
+   */
+  get_sub_issues: function(context) {
+    // Default context
+    context = context || document.body;
+
+    var issues = [];
+
+    $(this.issue_link_selector, context).each(function() {
+      if ($(this).text().match(issue_tree.issue_link_selector_regex)) {
+        issues.push($(this).attr('href').replace('\/node\/', ''));
+      }
+    });
+
+    return issues;
+  },
+
+  /**
+   * Appends iframes to the body with the issue pages.
+   *
+   * @param issues array
+   *   Array of issue ids.
+   */
+  create_issue_iframes: function(issues) {
+    for(idx in issues) {
+      var issue = issues[idx]
+      $('body').append('<iframe id="issue-'+ issue +'" src="/node/'+ issue +'#no_issue_tree" width="300px" height="300px"></iframe>');
+    }
+  }
+
+}
+
+var issues = issue_tree.get_sub_issues();
+issue_tree.create_issue_iframes(issues);
+
