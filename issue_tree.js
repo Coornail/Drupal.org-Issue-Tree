081359bda22fd74a34a1f667cb1c02a3e8e33cbf
Kornel Lugosi <coornail@gmail.com>
2011-08-26 16:12:59 +0100

    Implement queue for processing the issues asynchronously

--- issue_tree.js
+++ issue_tree.js
@@ -1,6 +1,8 @@
 var issue_tree = {
   /**
    * Parses the issue body searching for drupal.org issues.
+   *
+   * @todo strip cookies for iframe
    */
 
   // jQuery selector for the issue body.
@@ -17,6 +19,27 @@ var issue_tree = {
   issue_link_selector_regex: '^#[0-9]*',
 
   /**
+   * All the sub nodes.
+   *
+   * [
+   *   2: {parent: null, state: 'parsed'},
+   *   3: {parent: 2, state: 'parsed'},
+   *   4: {parent: 2, state: 'processing'},
+   *   173: {parent: 3, state: 'pending'},
+   * ]
+   */
+  sub_nodes : [],
+
+  /**
+   * Flag to identify when the parsing is finished.
+   * Possible values:
+   *   - idle
+   *   - processing
+   *   - finished
+   */
+  state: 'idle',
+
+  /**
    * Get the issue numbers from the issue body.
    *
    * @return array
@@ -28,11 +51,12 @@ var issue_tree = {
 
     var issues = [];
 
-    $(this.issue_link_selector, context).each(function() {
+    $(context).find(this.issue_link_selector).each(function() {
       if ($(this).text().match(issue_tree.issue_link_selector_regex)) {
         issues.push($(this).attr('href').replace('\/node\/', ''));
       }
     });
+
 
     return issues;
   },
@@ -84,10 +108,78 @@ var issue_tree = {
   },
 
   /**
+   * Returns the jQuery object for the subissue iframe.
+   */
+  get_subissue_iframe: function(id) {
+    return $($('#issue-' + id)[0].contentDocument);
+  },
+
+  /**
    * @todo implement
    */
-  parse_sub_issues: function(context) {
-    return [];
+  build_issue_process_queue: function(parent_, context) {
+    var issues = [];
+
+    var issue_numbers  = issue_tree.get_sub_issues();
+
+    for (idx in issue_numbers) {
+      var issue_number = issue_numbers[idx];
+      if (!issue_tree.sub_nodes[issue_number]) {
+        issue_tree.sub_nodes[issue_number] = {id: issue_number, parent: parent_, state: 'pending'};
+      }
+    }
+
+    return issues;
+  },
+
+  process_sub_issues: function() {
+    setTimeout(function() {
+      var found_issue_to_process = false;
+
+      for (idx in issue_tree.sub_nodes) {
+        if (issue_tree.sub_nodes[idx].state == 'pending' && issue_tree.state != 'finished') {
+          console.log('Processing '+ issue_tree.sub_nodes[idx].id);
+          console.log(issue_tree.sub_nodes);
+          issue_tree.state = 'processing';
+          issue_tree.process_sub_node(issue_tree.sub_nodes[idx]);
+          found_issue_to_process = true;
+        }
+      }
+
+      if (!found_issue_to_process) {
+        issue_tree.state = 'finished';
+        console.log(issue_tree.sub_nodes);
+      } else {
+        issue_tree.process_sub_issues();
+      }
+    },
+    3000);
+  },
+
+  /**
+   *
+   */
+  process_sub_node: function(issue) {
+
+    issue.state = 'pending';
+    $('body').append('<iframe id="issue-'+ issue.id +'" />');
+    $('#issue-' + issue.id).attr('src', '/node/'+ issue.id + '#no-issue-tree').hide();
+
+    $('#issue-' + issue.id).load(function() {
+      issue.state = 'processed';
+      issue_tree.state = 'idle';
+
+      var children = issue_tree.get_sub_issues(issue_tree.get_subissue_iframe(issue.id));
+      for (idx in children) {
+        child = children[idx]
+        if (!issue_tree.sub_nodes[child]) {
+          //issue_tree.sub_nodes[child] = {id: children[child], parent: issue.id, state: 'pending'};
+        }
+      }
+      console.log(issue_tree.sub_nodes);
+
+      $('#issue-'+ issue).remove();
+    });
   },
 
   /**
@@ -115,18 +207,20 @@ var issue_tree = {
     context = context || document.body;
     var issue = {};
 
-    issue['id'] = issue_tree.get_node_id(context);
+    issue['id'] = issue_tree.get_node_id();
+    issue['name'] = issue_tree.get_node_id(context);
     issue['title'] = issue_tree.get_title(context);
     issue['data'] = issue_tree.get_summary(context);
-    issue['children'] = issue_tree.parse_sub_issues(context);
+    //issue['children'] = issue_tree.parse_sub_issues(context);
+    issue_tree.build_issue_process_queue(issue['id']);
+    issue_tree.process_sub_issues();
 
     return issue;
   }
 
 }
 
-var issues = issue_tree.get_sub_issues();
-var issue = issue_tree.build_issue_object();
-console.log(issue);
-
-issue_tree.create_issue_iframes(issues);
+if (!window.location.pathname.match('#no-issue-tree')) {
+  $('body').css('background', 'red');
+  var issue = issue_tree.build_issue_object();
+}
